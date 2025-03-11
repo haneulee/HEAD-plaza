c188d310a1869a53efefb584e46e572a1eb06e01
Haneul Lee <lovesky4294@gmail.com>
2025-03-11 13:30:17 +0100

    fix: fix build errors

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  api: {
+    bodyParser: {
+      sizeLimit: "50mb",
+    },
+    responseLimit: "50mb",
+  },
+};
+
+module.exports = nextConfig;
