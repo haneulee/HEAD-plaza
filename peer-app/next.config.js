9ca3add178c97f34393305fa386a2be6149412b4
Haneul Lee <lovesky4294@gmail.com>
2025-03-11 14:14:46 +0100

    feat: update layout in mobile

--- peer-app/next.config.js
+++ peer-app/next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  api: {
-    bodyParser: {
-      sizeLimit: "50mb",
-    },
-    responseLimit: "50mb",
-  },
+  // api: {
+  //   bodyParser: {
+  //     sizeLimit: "50mb",
+  //   },
+  //   responseLimit: "50mb",
+  // },
   images: {
     domains: ["res.cloudinary.com"],
   },
--- peer-app/src/app/dolly-zoom-camera/page.tsx
+++ peer-app/src/app/dolly-zoom-camera/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useRef, useState } from "react";
 
+import Image from "next/image";
 import Peer from "peerjs";
 
 const PEER_ID = "dolly-zoom-camera";
@@ -503,29 +504,13 @@ const PeerPage = () => {
       {isProcessing && (
         <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
           <div className="flex flex-col items-center gap-4">
-            <svg
-              className="animate-spin h-12 w-12 text-white"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"
-              />
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              />
-            </svg>
-            <span className="text-white text-lg font-semibold">
-              Processing...
-            </span>
+            <Image
+              src="/loading.svg"
+              alt="uploading..."
+              width={100}
+              height={24}
+              priority
+            />
           </div>
         </div>
       )}
