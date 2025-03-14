7bc95393df876f1448b5d21c81ecb4acc730d5c4
Haneul Lee <lovesky4294@gmail.com>
2025-03-14 11:50:24 +0100

    feat: short unique id

--- peer-app/src/utils/generateUniqueId.ts
+++ peer-app/src/utils/generateUniqueId.ts
@@ -1,4 +1,4 @@
 // 고정 ID 대신 랜덤 ID 생성 함수 추가
 export const generateUniqueId = (): string => {
-  return Math.random().toString(36).substring(2, 15);
+  return Math.random().toString(36).substring(2, 7);
 };
